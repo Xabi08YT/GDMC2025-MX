0cf97c72520836bd70443d122b6b0852eace91ef
mateoguidi <mateo.guidi5@gmail.com>
2025-04-14 08:53:45 +0100

    Water well experiment updated

--- Experiments/waterWell.py
+++ Experiments/waterWell.py
@@ -1,17 +1,16 @@
 import time
-
 from gdpc import Editor, nbt_tools, interface, Block
 import os
 import threading
 
-host = 'https://gdmc.xabigoity.fr/'
+#host = 'https://gdmc.xabigoity.fr/'
 
-editor = Editor(buffering=False, host=host)
+editor = Editor(buffering=False)
 
 print("Placing water well...")
 pos = (100, 100, -10)
-waterwell_nbt = nbt_tools.parseNbtFile(os.path.abspath("schematics/oak_waterwell.nbt"))
-interface.placeStructure(waterwell_nbt, position=pos,host=host)
+waterwell_nbt = nbt_tools.parseNbtFile(os.path.abspath("oak_waterwell.nbt"))
+interface.placeStructure(waterwell_nbt, position=pos)
 
 print("Placing cauldron.")
 cauldron_pos = (103, 99, -7)
@@ -21,33 +20,35 @@ chaudron_x, chaudron_y, chaudron_z = cauldron_pos
 
 def monitor_cauldron():
     while True:
-        block = editor.getBlock((chaudron_x, chaudron_y, chaudron_z))
+        try:
+            block = editor.getBlock((chaudron_x, chaudron_y, chaudron_z))
 
-        print(block)
-        if ("water_cauldron" in str(block.id) or "cauldron" in str(block.id) and "level=3" not in str(block)):
-            print("Le chaudron a été utilisé ! Animation du mécanisme...")
-            level = editor.getBlock(chaudron_x, chaudron_y, chaudron_z).states
-            editor.placeBlock((chaudron_x, chaudron_y, chaudron_z), Block("air"))
+            if ("cauldron" in str(block.id) and block.states['level'] != 3):
+                print("Cauldron has been used! Animating mechanism...")
+                cauldron = editor.getBlock(cauldron_pos)
+                level = "1"
+                if (cauldron.id == "water_cauldron"):
+                    level = str(cauldron.states['level'])
+                if level == "0":
+                    level = "1"
+                editor.placeBlock((chaudron_x, chaudron_y, chaudron_z), Block("air"))
 
-            for y in range(chaudron_y, chaudron_y - 5, -1):
-                editor.placeBlock((chaudron_x, y, chaudron_z), Block("cauldron"))
-                time.sleep(0.5)
-                if y != chaudron_y - 5:
-                    editor.placeBlock((chaudron_x, y, chaudron_z), Block("chain"))
+                for y in range(chaudron_y, chaudron_y - 5, -1):
+                    editor.placeBlock((chaudron_x, y, chaudron_z), Block("water_cauldron[level=" + level + "]"))
+                    if y != chaudron_y - 5:
+                        editor.placeBlock((chaudron_x, y, chaudron_z), Block("chain"))
 
-            editor.placeBlock((chaudron_x, chaudron_y - 5, chaudron_z), Block("water_cauldron[level=3]"))
-            time.sleep(1)
+                editor.placeBlock((chaudron_x, chaudron_y - 5, chaudron_z), Block("water_cauldron[level=3]"))
 
-            for y in range(chaudron_y - 5, chaudron_y + 1, 1):
-                if y != chaudron_y - 5:
-                    editor.placeBlock((chaudron_x, y, chaudron_z), Block("water_cauldron[level=3]"))
-                    time.sleep(0.5)
-                if y != chaudron_y:
-                    editor.placeBlock((chaudron_x, y - 1, chaudron_z), Block("air"))
+                for y in range(chaudron_y - 5, chaudron_y + 1, 1):
+                    if y != chaudron_y - 5:
+                        editor.placeBlock((chaudron_x, y, chaudron_z), Block("water_cauldron[level=3]"))
+                    if y != chaudron_y:
+                        editor.placeBlock((chaudron_x, y - 1, chaudron_z), Block("air"))
+        except Exception as e:
+            print(f"Error in monitor_cauldron: {e}")
 
-            print("Le chaudron est prêt à être utilisé à nouveau !")
-
-        time.sleep(2)
+        time.sleep(0.5)
 
 
 monitor_thread = threading.Thread(target=monitor_cauldron, daemon=True)
@@ -57,4 +58,4 @@ try:
     while True:
         time.sleep(1)
 except KeyboardInterrupt:
-    print("Stoppé.")
+    print("Program terminated by user")
