142f505dece744bf25ee011ff216e1a2a49ddf87
Xabi Vittorio GOÏTY <xabigoity@gmail.com>
2025-04-17 11:36:20 +0900

    Now getting blocks from the real map

--- src/main.py
+++ src/main.py
@@ -1,13 +1,25 @@
 from json import load
 
-from src.Agent import Agent
+from Agent import Agent
+from Map import Map
+from gdpc import Editor, interface, vector_tools
 
 with open("config.json", mode="r") as cfg:
     config = load(cfg)
     cfg.close()
 
-agents = []
-for i in cfg[1:]:
-    for j in range(i):
-        agents.append(Agent())
+editor = Editor(buffering=True)
+buildArea = editor.getBuildArea()
 
+def getMcMap():
+    size = (buildArea.end[0] - buildArea.begin[0], buildArea.end[1] - buildArea.begin[1], buildArea.end[2] - buildArea.begin[2])
+    blocks = interface.getBlocks(buildArea.begin,size)
+    return blocks
+
+mcmap = Map(getMcMap())
+print(mcmap.get_block(1,1,1))
+print(mcmap.get_blocks(1,1,1,4,4,4))
+agents = []
+for i in cfg:
+    for j in range(i):
+        agents.append(Agent(mcmap))
