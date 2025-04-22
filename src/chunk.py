b37dbc2c1d35fd50a3cfd74124287cd8fb121152
Xabi Vittorio GOÏTY <xabigoity@gmail.com>
2025-04-22 16:16:04 +0900

    Added relative block scanning

--- src/chunk.py
+++ src/chunk.py
@@ -9,6 +9,8 @@ from src.utils import same_point
 
 excluded_files = ["areaData.json", "config.json"]
 
+files = {}
+
 def pull_chunk(args):
     tmp = interface.getBlocks(
         (args[0].begin[0] + args[1] * 16, -64, buildArea.begin[2] + args[2] * 16),
@@ -63,10 +65,8 @@ def pull_mc_map(buildArea, forceReload=False):
     print("Minecraft world pulled in {:.2f} seconds.".format(end - start))
 
 def push_chunk(file):
-    print(f"Pushing {file[0]} to minecraft world...")
-    with open(file[1], mode="r") as data:
-        chunk = json.load(data)
-        data.close()
+    print(f"Pushing {file} to minecraft world...")
+    chunk = json.load(files[file])
 
     formatted = [
         (
@@ -78,24 +78,55 @@ def push_chunk(file):
     interface.placeBlocks(formatted, doBlockUpdates=False)
 
 def push_mc_map():
+    if len(files.keys()) == 0:
+        load_all_files()
     start = time()
-    files = [(file,os.path.join(os.getcwd(), "data", file))
-             for file in os.listdir(os.path.join(os.getcwd(), "data"))
-             if
-                (not os.path.isdir(os.path.join(os.getcwd(), "data", file))
-                and file.endswith(".json")
-                and not file in excluded_files)
-             ]
-
     p = Pool(cpu_count())
-    p.map_async(push_chunk, files)
+    t = [key for key in files.keys()]
+    p.map_async(push_chunk, t)
 
     p.close()
     p.join()
     end = time()
     print("Minecraft world pushed in {:.2f} seconds".format(end - start))
 
-def scan(x, y, z, scan):
+def load_all_files():
+    global files
+    tmp = [(file, os.path.join(os.getcwd(), "data", file))
+             for file in os.listdir(os.path.join(os.getcwd(), "data"))
+             if
+             (not os.path.isdir(os.path.join(os.getcwd(), "data", file))
+              and file.endswith(".json")
+              and not file in excluded_files)
+             ]
+    for f,fp in tmp:
+        files[f.split(".")[0]] = open(fp, mode="r")
+
+def scan(x, y, z, radius, buildArea):
+    chunk = ((x-buildArea.begin[0])//16, (z-buildArea.begin[2])//16)
+    chunkdata = json.load(files[f"{chunk[0]}_{chunk[1]}"])
+    relatives = [(-1,-1),(-1,0),(-1,1),
+                 (0,-1), (0,1),
+                 (1,-1), (1,0), (1,1)]
+    for r in relatives:
+        try:
+            tmp = json.load(files[f"{chunk[0]+r[0]}_{chunk[1]+r[1]}"])
+            chunkdata.update(tmp)
+        except KeyError:
+            pass
+    res = {}
+    if len(files.keys()) == 0:
+        load_all_files()
+    for i in range(x-radius,x+radius+1):
+        for j in range(y-radius,y+radius+1):
+            for k in range(z-radius,z+radius+1):
+                b = chunkdata[str((i,j,k))]
+                res[str((i,j,k))] = Block(b[0], b[1], b[2])
+    return res
+
+def close_all_files():
+    for f in files:
+        files[f].close()
 
 
 if __name__ == "__main__":
@@ -103,4 +134,7 @@ if __name__ == "__main__":
     buildArea = current_editor.getBuildArea()
 
     pull_mc_map(buildArea)
-    push_mc_map()
+    load_all_files()
+    print(scan(-538,6,123,1,buildArea))
+    #push_mc_map()
+    close_all_files()
