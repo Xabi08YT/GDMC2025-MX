426bea72bddbca059d475220c6d510ca872b9f01
MateoGuidi <mateo.guidi5@gmail.com>
2025-04-22 08:28:38 +0200

    see around function done

--- src/utils.py
+++ src/utils.py
@@ -16,11 +16,10 @@ def distance_xz(ax: float, az: float, bx: float, bz: float)-> float:
 
 def get_ground_height(x: int, y_start: int, z: int) -> int:
     for y in range(y_start, 0, -1):
-        block = [] # block = getBlock from JSON
+        block = [] # block = getBlock from JSON using x, y and z
         if block[0] != "minecraft:air" and not block[0].endswith("leaves"):
             return y
     return 100
-
 
 def is_flat(x: int, y: int, z: int, radius: int = 2) -> float:
     heights = []
@@ -30,6 +29,16 @@ def is_flat(x: int, y: int, z: int, radius: int = 2) -> float:
             heights.append(ny)
     variation = max(heights) - min(heights)
     return 1 - min(variation / 5, 1)
+
+def see_around(x: int, z: int, radius: int = 6):
+    blocks = []
+    for dx in range(-radius, radius + 1):
+        for dz in range(-radius, radius + 1):
+            new_x, new_z = x + dx, z + dz
+            block = [] # block = getBlock from JSON using new_x and new_z
+            if block[0] != "minecraft:air" and not block[0].endswith("leaves"):
+                blocks.append((block, (new_x, new_z)))
+    return blocks
 
 def same_point(p1,p2):
     return p1[0] == p2[0] and p1[1] == p2[1] and p1[2] == p2[2]
