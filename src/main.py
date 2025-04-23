e6ac702548367d63a65f496ebeac6df6d6b2b01e
Xabi Vittorio GOÏTY <xabigoity@gmail.com>
2025-04-23 15:47:18 +0900

    Multiprocessing agents

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from json import load
 import random
+from multiprocessing import Pool
+from os import cpu_count
 from time import sleep
 from gdpc import Editor, Block
 from Agent import Agent
@@ -15,9 +17,9 @@ with open("config.json", mode="r") as cfg:
 utils.current_editor = Editor(buffering=True)
 buildArea = utils.current_editor.getBuildArea()
 print("GDMC 2025 MX - Prototype")
-utils.current_editor.runCommand('tellraw @a "GDMC - Prototype"')
+utils.current_editor.runCommand('tellraw @a [{"text":"GDMC","color":"cyan"},{"text":" - Prototype","color":"white"}]')
 print("Fetching map from Minecraft...")
-utils.current_editor.runCommand('tellraw @a "GDMC - Map extraction started..."')
+utils.current_editor.runCommand('tellraw @a [{"text":"GDMC","color":"cyan"},{"text":" - Map extraction started. It may take several minutes.","color":"white"}]')
 pull_mc_map()
 
 
@@ -25,31 +27,28 @@ pull_mc_map()
 villageCenter = (random.randint(buildArea.begin[0],buildArea.end[0]), random.randint(buildArea.begin[2],buildArea.end[2]))
 center_pos = (villageCenter[0], utils.get_ground_height(villageCenter[0], 200, villageCenter[1])+1, villageCenter[1])
 utils.current_editor.placeBlock(center_pos, Block("minecraft:campfire"))
-utils.current_editor.runCommand('tellraw @a "GDMC - Map extraction done, simulation started..."')
+utils.current_editor.runCommand('tellraw @a [{"text":"GDMC","color":"cyan"},{"text":" - Map extraction done, simulation started.","color":"white"}]')
 
 for i in range(config["nodeAgents"][0]):
     utils.agents.append(Agent(center_pos[0], center_pos[1], center_pos[2], villageCenter))
 
-threads = []
-for agent in utils.agents:
-    thread = threading.Thread(target=agent.tick)
-    threads.append(thread)
-    print("Agent " + agent.name + " started")
-    thread.start()
+def processAgent(agent: Agent):
+    print("Starting agent " + agent.name)
+    for _ in range(100):
+        agent.tick()
+        sleep(0.1)
+    return
 
-sleep(10)
+
+p = Pool(cpu_count())
+print(p.map_async(processAgent, utils.agents).get())
+p.close()
+p.join()
 
 print("Simulation stopped, let's see the progress of the agents")
 
-for agent in utils.agents:
-    agent.tickEnable = False
-    print("Agent " + agent.name + " is " + agent.current_phase)
-
-for thread in threads:
-    thread.join()
-
-utils.current_editor.runCommand('tellraw @a "GDMC - Simulation stopped, pushing new map to Minecraft.."')
+utils.current_editor.runCommand('tellraw @a [{"text":"GDMC","color":"cyan"},{"text":" - Pushing new map. It may tell several minutes.","color":"white"}]')
 print("Simulation stopped, pusing new to map to Minecraft..")
 push_mc_map()
-utils.current_editor.runCommand('tellraw @a "GDMC - Map pushed, simulation finished"')
+utils.current_editor.runCommand('tellraw @a [{"text":"GDMC","color":"cyan"},{"text":" - Map pushed. The program has now ended.","color":"white"}]')
 print("Simulation finished, let's see the modifications in Minecraft")
