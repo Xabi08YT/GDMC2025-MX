1e221963fd3f58c7678a8a6a8f052ea9b570a5e3
MateoGuidi <mateo.guidi5@gmail.com>
2025-04-24 08:58:00 +0200

    relationships added, agents makes their life

--- src/Agent.py
+++ src/Agent.py
@@ -1,39 +1,48 @@
-from uuid import uuid4
 import random
+from uuid import uuid4
+from random import choice
 from Job import JobType, Job
-
+from gdpc import Block
+from gdpc.vector_tools import ivec3
+from utils import distance_xz, agents
 
 class Agent:
-    def __init__(self, x: int = 0, y: int = 100, z: int=0, center_village: tuple = (0, 0), job: Job = JobType.UNEMPLOYED):
+    def __init__(self, world: list[tuple[ivec3, Block]], radius: int = 20, x: int = 0, y: int = 100, z: int=0, center_village: tuple = (0, 0), job: Job = JobType.UNEMPLOYED):
         self.id: str = str(uuid4())
+        self.world: list[tuple[ivec3, Block]] = world
+        self.radius = radius
         self.x: float = x
         self.y: float = y
         self.z: float = z
         self.center_village: tuple = center_village
         self.job = job
         self.tickEnable = True
+        self.basics_needs = {
+            "hunger": 1.0,
+            "social": 0.8,
+            "energy": 1.0,
+            "health": 1.0,
+            "chill": 0.5,
+        }
         self.needs = {
             "hunger": 1.0,
             "social": 0.8,
             "energy": 1.0,
             "health": 1.0,
-            #"farfromothers": random.uniform(-1, 1),
-            #"farfromcenter": random.uniform(-1, 1),
-            #"flatspace": random.uniform(-1, 1)
         }
         self.needs_decay = {
-            "hunger": 0.03,
-            "energy": 0.02,
-            "social": 0.05,
-            "health": 0.01,
+            "hunger": round(random.uniform(0.00, 0.1), 2),
+            "energy": round(random.uniform(0.00, 0.1), 2),
+            "social": round(random.uniform(0.00, 0.1), 2),
+            "health": round(random.uniform(0.00, 0.1), 2),
         }
         self.attributes = {}
-        self.relationships = {}
+        self.relationships = []
         self.actions = {}
         self.observations = {}
         self.current_phase = "starting"
         with open("./txt/agent_names.txt", "r") as f:
-            self.name = random.choice(f.readlines()).strip()
+            self.name = choice(f.readlines()).strip()
 
     def __repr__(self):
         return "Agent(id={}, x={}, y={}, z={})".format(self.id, self.x, self.y, self.z)
@@ -47,27 +56,85 @@ class Agent:
     def get_position(self) -> tuple:
         return self.x, self.y, self.z
 
+    def set_need(self, need: str, increment: float):
+        new_need = self.needs[need] + increment
+        if (new_need >= 0 and new_need <= 1):
+            self.needs[need] = new_need
+
+    # -- ACTIONS --
+
+    def eat(self):
+        self.current_phase = "eating"
+        self.set_need("energy", -self.needs_decay["energy"])
+        self.set_need("health", -self.needs_decay["health"])
+        self.set_need("hunger", round(random.uniform(0.8, 1.0), 2))
+        self.set_need("social", -self.needs_decay["social"])
+
+    def social(self):
+        self.current_phase = "socializing"
+        self.set_need("energy", -self.needs_decay["energy"])
+        self.set_need("health", self.needs_decay["health"])
+        self.set_need("hunger", -self.needs_decay["hunger"])
+        self.set_need("social", self.needs_decay["social"])
+
+        other_agent = min([agent for agent in agents if agent.id != self.id], key=lambda agent: distance_xz(self.x, self.z, agent.x, agent.z))
+
+        dx = (other_agent.x - self.x) * 0.5
+        dz = (other_agent.z - self.z) * 0.5
+        self.move(dx, 0, dz)
+
+        if distance_xz(self.x, self.z, other_agent.x, other_agent.z) > 5:
+            pass
+
+        if other_agent.name not in self.relationships:
+            self.relationships.append(other_agent.name)
+
+        print(f"{self.name} socialized with {other_agent.name}")
+
+    def sleep(self):
+        self.current_phase = "sleeping"
+        self.set_need("energy", round(random.uniform(0.8, 1.0), 2))
+        self.set_need("health", round(random.uniform(0.15, 0.4), 2))
+        self.set_need("hunger", -self.needs_decay["hunger"])
+        self.set_need("social", -self.needs_decay["social"])
+
+    def rest(self):
+        self.current_phase = "resting"
+        self.set_need("energy", round(random.uniform(0.3, 0.5), 2))
+        self.set_need("health", round(random.uniform(0.1, 0.3), 2))
+        self.set_need("hunger", -self.needs_decay["hunger"])
+        self.set_need("social", -self.needs_decay["social"])
+
+    def move(self, increment_x: float, increment_y: float, increment_z: float):
+        self.current_phase = "moving"
+        new_x = self.x + increment_x
+        new_z = self.z + increment_z
+        if (new_x <= self.radius and new_x >= -self.radius):
+            self.x = new_x
+        if (new_z <= self.radius and new_z >= -self.radius):
+            self.z = new_z
+        self.set_need("hunger", -self.needs_decay["hunger"])
+        self.set_need("energy", -self.needs_decay["energy"])
+        self.set_need("health", self.needs_decay["health"])
+        self.set_need("social", -self.needs_decay["social"])
+        print(f"{self.name}'s pos = {self.x}, {self.z}")
+
+    # -- TICK --
+
     def tick(self):
         if not self.tickEnable:
             return
 
-        for need in self.needs:
-            self.needs[need] = max(0, self.needs[need] - self.needs_decay[need])
-
-        priority_need = min(self.needs)
+        priority_need = min(self.needs, key=self.needs.get)
 
         if priority_need == "hunger":
-            #eat
-            pass
+            self.eat()
         elif priority_need == "social":
-            #social
-            pass
+            self.social()
         elif priority_need == "energy":
-            #sleep
-            pass
+            self.sleep()
         elif priority_need == "health":
-            #rest
-            pass
+            self.move(random.randint(-5, 5), 0, random.randint(-5, 5))
         else:
-            #explore
-            pass
+            self.move(random.randint(-5, 5), 0, random.randint(-5, 5))
+        print(self.name + " is " + self.current_phase)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
+from concurrent.futures import ThreadPoolExecutor
 from json import load
 from time import sleep
 from gdpc import interface
 from Agent import Agent
-import utils
+from utils import agents
+from random import randint
 
 with open("config.json", mode="r") as cfg:
     config = load(cfg)
@@ -12,22 +14,34 @@ print("GDMC 2025 MX - Prototype")
 print("Fetching map from Minecraft...")
 ba = interface.getBuildArea()
 offset, size = ba.offset, ba.size
-blocks = interface.getBlocks(offset, size, "overworld", True)
+map = interface.getBlocks(offset, size, "overworld", True)
+radius = config["radius"]
 
 for i in range(config["nodeAgents"][0]):
-    utils.agents.append(Agent())
+    x = randint(-radius, radius)
+    z = randint(-radius, radius)
+    agents.append(Agent(map, radius, x, 0, z))
 
 def processAgent(agent: Agent):
     print("Starting agent " + agent.name)
     for _ in range(100):
         agent.tick()
-        sleep(0.5)
+        sleep(0.1)
     return
+
+with ThreadPoolExecutor() as executor:
+    executor.map(processAgent, agents)
 
 print("Simulation stopped, let's see the progress of the agents")
 
-for agent in utils.agents:
+for agent in agents:
     agent.tickEnable = False
-    print("Agent " + agent.name + " is " + agent.current_phase)
+    print("----")
+    print("Agent " + agent.name + " - " + agent.current_phase)
+    print("Hunger: " + str(agent.needs["hunger"]) + "(" + str(agent.needs_decay["hunger"]) + ")")
+    print("Energy: " + str(agent.needs["energy"]) + "(" + str(agent.needs_decay["energy"]) + ")")
+    print("Social: " + str(agent.needs["social"]) + "(" + str(agent.needs_decay["social"]) + ")")
+    print("Health: " + str(agent.needs["health"]) + "(" + str(agent.needs_decay["health"]) + ")")
+    print("Relationships: " + str(agent.relationships))
 
 print("Simulation done")
