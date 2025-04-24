6ae4df7b1861b0e1c2e5393d3ce43af6dc5d7020
Xabi Vittorio GOÏTY <xabigoity@gmail.com>
2025-04-24 15:50:04 +0900

    AbstractionLayer V1

---
+++ src/AbstractionLayer.py
@@ -0,0 +1,85 @@
+from src.Chunk import Chunk
+from time import time
+from gdpc import interface, Editor
+from multiprocessing import Pool, cpu_count
+import os, json, utils
+
+
+class AbstractionLayer:
+
+    _AbstractionLayerInstance = None
+
+    def __init__(self, buildArea: interface.Box):
+        if(AbstractionLayer._AbstractionLayerInstance is not None):
+            raise RuntimeError("AbstractionLayer._AbstractionLayerInstance already initialized")
+        AbstractionLayer._AbstractionLayerInstance = self
+        self.buildArea = buildArea
+
+    @staticmethod
+    def pull_chunk(args: tuple[interface.Box,int,int]) -> None:
+        tmp = interface.getBlocks(
+            (args[0].begin[0] + args[1] * 16, -64, args[0].begin[2] + args[2] * 16),
+            (16, 385, 16)
+        )
+
+        chunk = Chunk.from_gdmc(tmp, f"{args[1]}_{args[2]}.json")
+
+        chunk.to_file(f"{chunk.name}.json")
+
+    def pull(self, forceReload:bool =False):
+        start = time()
+        print("Pulling minecraft world...")
+        size = (self.buildArea.end[0] - self.buildArea.begin[0], self.buildArea.end[1] - self.buildArea.begin[1],
+                self.buildArea.end[2] - self.buildArea.begin[2])
+        if os.path.exists(os.path.join(os.getcwd(), "data", "areaData.json")) and not forceReload:
+            try:
+                with open(os.path.join(os.getcwd(), "data", "areaData.json"), "r") as f:
+                    data = json.load(f)
+                    f.close()
+                if utils.same_point(self.buildArea.begin, data["begin"]) and utils.same_point(self.buildArea.end, data["end"]):
+                    print("Same area, skipping world pulling... If you want to pull it, remove the folder.")
+                    return
+            except json.JSONDecodeError:
+                print("Invalid JSON, loading everything...")
+            except KeyError:
+                print("Invalid JSON, loading everything...")
+
+        if os.path.exists(os.path.join(os.getcwd(), "data")):
+            for filename in os.listdir(os.path.join(os.getcwd(), "data")):
+                os.remove(os.path.join(os.getcwd(), "data", filename))
+            os.rmdir(os.path.join(os.getcwd(), "data"))
+        os.mkdir(os.path.join(os.getcwd(), "data"))
+
+        chunks_grid = [(self.buildArea, x, y) for x in range(size[0] // 16 + 1) for y in range(size[2] // 16 + 1)]
+
+        p = Pool(cpu_count())
+        p.map_async(AbstractionLayer.pull_chunk, chunks_grid)
+        p.close()
+        p.join()
+
+        with open(os.path.join(os.getcwd(), "data", "areaData.json"), "w+") as f:
+            json.dump({"begin": self.buildArea.begin.to_list(), "end": self.buildArea.end.to_list()}, f)
+            f.close()
+        end = time()
+        print("Minecraft world pulled in {:.2f} seconds.".format(end - start))
+
+    def get_chunk(self,x,z) -> Chunk:
+        cid = Chunk.get_chunk_id_from_coord(self.buildArea, x, z)
+        return Chunk.from_file(f"{cid[0]}_{cid[1]}.json")
+
+    def save_all(self):
+        for chunk in Chunk.LOADED_CHUNKS.keys():
+            Chunk.LOADED_CHUNKS[chunk].to_file(filename=f"{chunk.name}.json", path=chunk.path)
+
+    def push(self, folder="generated"):
+        self.save_all()
+        for file in os.listdir(folder):
+            if file.endswith(".json"):
+                c = Chunk.from_file(file,folder)
+                interface.placeBlocks(c.to_gdmc(), doBlockUpdates=False)
+
+if __name__ == "__main__":
+    editor = Editor(buffering=True)
+    abl = AbstractionLayer(editor.getBuildArea())
+    abl.pull()
+    abl.save_all()
---
+++ src/Chunk.py
@@ -0,0 +1,95 @@
+import json
+import os
+
+from gdpc import Block, interface
+from pyglm.glm import ivec3
+from uuid import uuid4
+
+
+class Chunk:
+
+    CHUNK_SIZE = 16
+    LOADED_CHUNKS = {}
+
+    def __init__(self,chunk: dict[str,Block], name: str = str(uuid4()), folder:str = "data"):
+        if chunk is None:
+            chunk = {}
+        self.chunk = chunk
+        self.name = name
+        self.folder = folder
+
+    def get_block(self,x,y,z) -> Block:
+        return self.chunk[Chunk.coord_to_key(x,y,z)]
+
+    def set_block(self,x:int ,y: int,z: int,block: Block) -> None:
+        self.chunk[Chunk.coord_to_key(x,y,z)] = block
+
+    def is_in_chunk(self, x:int, y:int, z:int) -> bool:
+        return Chunk.coord_to_key(x,y,z) in self.chunk.keys()
+
+    @staticmethod
+    def serialize(data: dict[str, tuple[str,dict,any]]):
+        res = {}
+        for coord, block in data.items():
+            res[coord] = Block(block[0], block[1], block[2])
+        return Chunk(res)
+
+    @staticmethod
+    def deserialize(chunk) -> dict[str, tuple[str,dict,any]]:
+        res = {}
+        for coord, block in chunk.chunk.items():
+            res[coord] = (block.id,block.states,block.data)
+        return res
+
+    @staticmethod
+    def from_gdmc(data: list[tuple[ivec3, Block]], name: str):
+        if name.replace(".json","") in Chunk.LOADED_CHUNKS.keys():
+            return Chunk.LOADED_CHUNKS[name.replace(".json","")]
+        cdata = {}
+        for coord, block in data:
+            if not "void_air" in block.id:
+                cdata[Chunk.ivec3_to_key(coord)] = block
+        c = Chunk(cdata, name)
+        Chunk.LOADED_CHUNKS[c.name] = c
+        return c
+
+    def to_gdmc(self):
+        return [(Chunk.key_to_coord(coord),block) for coord,block in self.chunk.items()]
+
+    @staticmethod
+    def from_file(filename: str, path: str = "data"):
+        if filename.replace(".json","") in Chunk.LOADED_CHUNKS.keys():
+            return Chunk.LOADED_CHUNKS[filename.replace(".json","")]
+
+        with open(os.path.join(os.getcwd(), path, filename),"r") as f:
+            data = json.load(f)
+            f.close()
+        c = Chunk.serialize(data)
+        c.name = filename.replace(".json","")
+        Chunk.LOADED_CHUNKS[c.name] = c
+        return c
+
+    def to_file(self, filename: str = None, path: str = "data"):
+        if filename is None:
+            filename = self.name + ".json"
+        with open(os.path.join(os.getcwd(), path, filename),"w+") as f:
+            json.dump(self.deserialize(self), f)
+            f.close()
+        Chunk.LOADED_CHUNKS.pop(self.name)
+        return
+
+    @staticmethod
+    def ivec3_to_key(coord: ivec3) -> str:
+        return str((coord[0], coord[1], coord[2]))
+
+    @staticmethod
+    def coord_to_key(x,y,z) -> str:
+        return str((x, y, z))
+
+    @staticmethod
+    def key_to_coord(key):
+        return key[1:-1].split(",")
+
+    @staticmethod
+    def get_chunk_id_from_coord(buildArea: interface.Box, x:int,z:int) -> tuple[int,int]:
+        return x - buildArea.begin[0], z - buildArea.begin[2]
