042aa3cd15c13f4d6b2acd2dc8cb4e65d85a61c2
Xabi Vittorio GOÏTY <xabigoity@gmail.com>
2025-04-28 14:35:38 +0900

    Orientation changes

--- src/Building.py
+++ src/Building.py
@@ -1,14 +1,17 @@
+from random import random, choice
+
 from gdpc.interface import ivec3
 import Agent
 from Chunk import Chunk
 import utils
+from src.utils import distance_xz
 
 
 class Building:
-
     BUILDINGS = []
 
-    def __init__(self, center_point: ivec3 | None, agent: Agent, name:str, orientation: str = "south", built: bool = False, folder="generated"):
+    def __init__(self, center_point: ivec3 | None, agent: Agent, name: str, orientation: str = "south",
+                 built: bool = False, folder="generated"):
         self.built = built
         self.orientation = orientation
         self.center_point = center_point
@@ -16,45 +19,48 @@ class Building:
         self.name = name
         self.folder = folder
         self.chunk = Chunk({}, name, folder)
-        self.radius = 20
+        self.radius = 10
         Building.BUILDINGS.append(self)
 
     def built(self):
         self.built = True
         print(f"Building at x={self.center_point[0]}, y={self.center_point[1]}, z={self.center_point[2]} done!")
 
-    def set_orientation_towards_center(self, agent: Agent = None):
+    def set_orientation_according_to_center(self, village_center: tuple[int, int], agent: Agent = None):
         if agent is None:
             agent = self.agent
 
         if self.center_point is None:
             return
 
-        dx = self.center_point.x - agent.center_village[0]
-        dz = self.center_point.z - agent.center_village[1]
+        orientations = {"north": [0, -1], "south": [0, 1], "east": [0, 1], "west": [0, -1]}
+        score = {k: distance_xz(village_center[0], village_center[1], self.center_point.x + self.radius * e[0], self.center_point.z + self.radius * e[1]) for k,e in orientations.items()}
 
-        if abs(dx) > abs(dz):
-            if dx > 0:
-                self.orientation = "west"
-            else:
-                self.orientation = "east"
-        else:
-            if dz > 0:
-                self.orientation = "north"
-            else:
-                self.orientation = "south"
+        max = -32768
+        K = None
+        for k,s in score.items():
+            if s > max:
+                max = s
+                K = k
+            elif s == max and random() < 0.5:
+                K = k
+
+        orientations.pop(K)
+        keys = list(orientations.keys())
+        self.orientation = orientations[choice(keys)]
 
     def __repr__(self):
         return "Building(center_point={}, agent={})".format(self.center_point, self.agent)
 
     def __str__(self):
-        return " at x={}, y={}, z={} owned by {}".format(self.center_point.x, self.center_point.y, self.center_point.z, self.agent.name)
+        return " at x={}, y={}, z={} owned by {}".format(self.center_point.x, self.center_point.y, self.center_point.z,
+                                                         self.agent.name)
 
     def detect_tresspassing(self, x, z):
-        return utils.distance_xz(self.center_point[0], self.center_point[2], x,z) <= self.radius
+        return utils.distance_xz(self.center_point[0], self.center_point[2], x, z) <= self.radius
 
     @staticmethod
-    def detect_all_tresspassing(x,z):
+    def detect_all_tresspassing(x, z):
         tresspassing = []
         for b in Building.BUILDINGS:
             if b.detect_tresspassing(x, z):
