785f412e64b7251af2faf992fdb02fb6768883df
Xabi Vittorio GOÏTY <xabigoity@gmail.com>
2025-05-13 10:23:44 +0900

    Better messages

--- src/Simulation.py
+++ src/Simulation.py
@@ -26,21 +26,27 @@ class Simulation:
             self.params = json.load(sim)
             sim.close()
 
+    def show_message(self,message):
+        editor = Editor(buffering=True)
+        print("[INFO] " + message)
+        editor.runCommand('tellraw @a [{"text":"GDMC","color":"aqua"},{"text":" - '+message+'","color":"white"}]')
+
+
     def prepare(self):
         print("GDMC 2025 - MX")
         editor = Editor(buffering=True)
         editor.runCommand('tellraw @a [{"text":"GDMC 2025 - MX","color":"aqua"}]')
         if not os.path.exists("generated"):
             os.mkdir("generated")
-        print("Fetching map from Minecraft...")
-        editor.runCommand(
-            'tellraw @a [{"text":"GDMC","color":"aqua"},{"text":" - Map extraction started. It may take several minutes.","color":"white"}]')
+        self.show_message("Pulling minecraft map... This may take several minutes...")
         ba = editor.getBuildArea()
         self.abl = AbstractionLayer(ba)
         try:
             self.abl.pull("--force-pull" in sys.argv or "-fp" in sys.argv)
         except:
             pass
+
+        self.show_message("Done. Preparing simulation...")
 
         self.min_x, self.min_z = ba.begin[0], ba.begin[2]
         self.max_x, self.max_z = ba.end[0] - 1, ba.end[2] - 1
@@ -53,6 +59,8 @@ class Simulation:
             agent = Agent(self, x, y, z)
             self.agents.append(agent)
 
+        self.show_message("Simulation ready.")
+
     def run(self, agent):
         agent.logfile = LogFile(fpath="logs/ongoing", fname=f"{str(agent.id)}.csv")
         for i in range(self.config["nbTurns"]):
@@ -61,34 +69,23 @@ class Simulation:
         agent.logfile.close()
 
     def launch(self):
-        editor = Editor(buffering=True)
-        editor.runCommand(
-            'tellraw @a [{"text":"GDMC","color":"aqua"},{"text":" - Starting simulation...","color":"white"}]')
-        print("Starting simulation...")
+        self.show_message("Simulation launched. This may take a while to complete.")
 
         p = Pool(cpu_count())
         p.map_async(self.run, self.agents).get()
         p.close()
         p.join()
 
-        editor = Editor(buffering=True)
-        editor.runCommand(
-            'tellraw @a [{"text":"GDMC","color":"aqua"},{"text":" - Simulation stopped. To see the evolution of the statistics, you can start the visualization server.","color":"white"}]')
-        print("Simulation stopped.")
+        self.show_message("Simulation ended.")
 
     def end(self):
 
-        editor = Editor(buffering=True)
-        editor.runCommand(
-            'tellraw @a [{"text":"GDMC","color":"aqua"},{"text":" - Pushing changes to the map... (This step may take several minutes)","color":"white"}]')
-        print("Pushing changes to the map...")
+        self.show_message("Pushing changes to Minecraft... This may take several minutes.")
 
         self.abl.save_all()
         self.abl.push()
 
-        editor.runCommand(
-            'tellraw @a [{"text":"GDMC","color":"aqua"},{"text":" - Done. Cleaning up...","color":"white"}]')
-        print("Changes pushed. Cleaning up...")
+        self.show_message("Changes pushed. Beginning cleanup...")
 
         for file in os.listdir("generated"):
             os.remove(f"generated/{file}")
@@ -103,17 +100,11 @@ class Simulation:
         os.rmdir("logs/ongoing")
 
         if "--visualize" in sys.argv or "-vs" in sys.argv:
-            editor.runCommand(
-                'tellraw @a [{"text":"GDMC","color":"aqua"},{"text":" - Done. Launching visualization server... Please check your default web browser.","color":"white"}]'
-            )
-            print("Done. Launching visualization server... Please check your default web browser.")
+            self.show_message("Done. Visualization server is now running. Please check your browser.")
             from visualization.VisualizeSim import launch_visualization_server
             launch_visualization_server()
         else:
-            editor.runCommand(
-                'tellraw @a [{"text":"GDMC","color":"aqua"},{"text":" - Done. Goodbye world !","color":"white"}]'
-            )
-            print("Done. Goodbye world !")
+            self.show_message("Done. Goodbye World !")
 
 if __name__ == "__main__":
     sim = Simulation()
