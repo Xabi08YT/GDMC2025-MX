6c2f55429759322f4ad6f1537615f649d5995a10
mateoguidi <mateo.guidi5@gmail.com>
2025-05-26 09:00:55 +0100

    all job buildings started and half done

--- src/buildings/BlacksmithBuilding.py
+++ src/buildings/BlacksmithBuilding.py
@@ -1,3 +1,4 @@
+import random
 from random import randint
 from math import inf
 
@@ -8,17 +9,31 @@ from utils.math_methods import distance_xz
 
 class BlacksmithBuilding(JobBuilding):
     def __init__(self, center_point: tuple[int,int,int] | None, agent, orientation: str = "north"):
-        super().__init__(center_point, agent, agent.name + "'s BlacksmithBuilding", orientation)
+        super().__init__(center_point, agent, agent.name + "'s BlacksmithBuilding", orientation, width=random.randint(5, 10), height=6, depth=random.randint(3, 10))
         if center_point is None:
             center_point = self.best_spot(20,agent.simulation)
         self.place(center_point,agent.simulation)
+        self.corners = [
+            [0, 0],
+            [0, self.depth - 1],
+            [self.width - 1, 0],
+            [self.width - 1, self.depth - 1]
+        ]
 
     def build(self):
-        for i in range(self.step):
-            self.add_block_to_matrix(self.center_point[0],
-                                        self.agent.simulation.heightmap[self.center_point[0], self.center_point[1]],
-                                        self.center_point[2], "minecraft:obsidian")
-
+        for dx in range(self.width):
+            for dz in range(self.depth):
+                self.add_block_to_matrix(dx, 4, dz, "minecraft:cobblestone")
+                self.add_block_to_matrix(dx, 0, dz, "minecraft:cobblestone")
+                if dx == 0 or dx == self.width - 1 or dz == 0 or dz == self.depth - 1:
+                    self.add_block_to_matrix(dx, 5, dz, "minecraft:smooth_stone_slab[type=bottom]")
+                if [dx, dz] in self.corners:
+                    for dy in range(1, 4):
+                        self.add_block_to_matrix(dx, dy, dz, "minecraft:stone_brick_wall")
+        for i in range(2):
+            self.add_block_to_matrix(1 + i, 1, 0, "minecraft:furnace[facing=south]")
+            self.add_block_to_matrix(3, 1, 0, "minecraft:anvil[facing=south]")
+            self.add_block_to_matrix(self.width - 2, 1, self.depth - 2, "minecraft:lava_cauldron")
         self.check_built()
 
     def best_spot(self, nbtry, simulation):
@@ -30,11 +45,12 @@ class BlacksmithBuilding(JobBuilding):
             x = randint(0,simulation.heightmap.shape[0])
             z = randint(0,simulation.heightmap.shape[1])
 
-            score = -simulation.water[x-self.width//2-1:x+self.width//2+1,z-self.depth//2-1,z+self.depth//2+1].sum()
+            score = -simulation.water[x-self.width//2-1:x+self.width//2+1,z-self.depth//2-1:z+self.depth//2+1].sum()
             score -= distance_xz(x,simulation.firecamp_coords[0],z,simulation.firecamp_coords[1])
-            score += 0.5 * simulation.lava[x-self.width:x+self.width,z-self.depth,z+self.depth]
+            score += 0.5 * simulation.lava[x-self.width:x+self.width,z-self.depth:z+self.depth].sum()
 
-            if simulation.walkable[x-self.width//2-1:x+self.width//2+1,z-self.depth//2-1,z+self.depth//2+1].count().item() < self.width * self.depth:
+            if simulation.walkable[x - self.width // 2 - 1:x + self.width // 2 + 1,
+               z - self.depth // 2 - 1:z + self.depth // 2 + 1].sum().item() < self.width * self.depth:
                 continue
 
             if score > best_score:
