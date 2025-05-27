cf277ecb675289aefa9354ebaace6b2aaf1f755d
Xabi Vittorio GOÏTY <xabigoity@gmail.com>
2025-05-27 09:54:33 +0900

    Merged final house algorythm

--- src/buildings/CommunityBuilding.py
+++ src/buildings/CommunityBuilding.py
@@ -1,5 +1,6 @@
 from math import inf
 from random import randint
+import random
 
 from buildings.JobBuilding import JobBuilding
 
@@ -8,16 +9,32 @@ from utils.math_methods import distance_xz
 
 class CommunityBuilding(JobBuilding):
     def __init__(self, center_point: tuple[int,int,int] | None, agent, orientation: str = "north"):
-        super().__init__(center_point, agent, agent.name + "'s CommunityBuilding", orientation)
+        super().__init__(center_point, agent, agent.name + "'s CommunityBuilding", orientation, width=random.randint(5, 10), height=6, depth=random.randint(3, 10))
         if center_point is None:
             center_point = self.best_spot(20,agent.simulation)
         self.place(center_point,agent.simulation)
+        self.corners = [
+            [0, 0],
+            [0, self.depth - 1],
+            [self.width - 1, 0],
+            [self.width - 1, self.depth - 1]
+        ]
 
     def build(self):
-        for i in range(self.step):
-            self.add_block_to_matrix(self.center_point[0],
-                                        self.agent.simulation.heightmap[self.center_point[0], self.center_point[1]],
-                                        self.center_point[2], "minecraft:bookshelf")
+        for dx in range(self.width):
+            for dz in range(self.depth):
+                self.add_block_to_matrix(dx, 4, dz, "minecraft:oak_planks")
+                self.add_block_to_matrix(dx, 0, dz, "minecraft:oak_planks")
+                if dx == 0 or dx == self.width - 1 or dz == 0 or dz == self.depth - 1:
+                    self.add_block_to_matrix(dx, 5, dz, "minecraft:oak_slab[type=bottom]")
+                    if [dx, dz] in self.corners:
+                        for dy in range(1, 4):
+                            self.add_block_to_matrix(dx, dy, dz, "minecraft:oak_fence")
+                else:
+                    self.add_block_to_matrix(dx, 1, dz, "minecraft:bookshelf")
+                    self.add_block_to_matrix(dx, 2, dz, "minecraft:bookshelf")
+                    self.add_block_to_matrix(dx, 3, dz, "minecraft:oak_trapdoor")
+
         self.check_built()
 
     def best_spot(self, nbtry, simulation):
--- src/buildings/WorkshopBuilding.py
+++ src/buildings/WorkshopBuilding.py
@@ -5,19 +5,32 @@ from buildings.JobBuilding import JobBuilding
 
 from utils.math_methods import distance_xz
 
+import random
+
 
 class WorkshopBuilding(JobBuilding):
     def __init__(self, center_point: tuple[int,int,int] | None, agent, orientation: str = "north"):
-        super().__init__(center_point, agent, agent.name + "'s WorkshopBuilding", orientation)
+        super().__init__(center_point, agent, agent.name + "'s WorkshopBuilding", orientation, width=random.randint(5, 10), height=6, depth=random.randint(3, 10))
         if center_point is None:
             center_point = self.best_spot(20,agent.simulation)
         self.place(center_point,agent.simulation)
+        self.corners = [
+            [0, 0],
+            [0, self.depth - 1],
+            [self.width - 1, 0],
+            [self.width - 1, self.depth - 1]
+        ]
 
     def build(self):
-        for i in range(self.step):
-            self.add_block_to_matrix(self.center_point[0],
-                                        self.agent.simulation.heightmap[self.center_point[0], self.center_point[1]],
-                                        self.center_point[2], "minecraft:clay")
+        for dx in range(self.width):
+            for dz in range(self.depth):
+                self.add_block_to_matrix(dx, 3, dz, "minecraft:stone_bricks")
+                self.add_block_to_matrix(dx, 0, dz, "minecraft:stone_bricks")
+                if [dx, dz] in self.corners:
+                    for dy in range(1, 3):
+                        self.add_block_to_matrix(dx, dy, dz, "minecraft:cobblestone_wall")
+        self.add_block_to_matrix(3, 1, 0, "minecraft:water_cauldron")
+        self.add_block_to_matrix(2, 1, 0, "minecraft:stonecutter[facing=south]")
         self.check_built()
 
     def best_spot(self, nbtry, simulation):
